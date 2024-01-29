d041e6d84962fad281277e318e5ba1deaac361e1
genuine-seok <genuineseok@gmail.com>
2024-01-29 16:15:22 +0900

    feat: 텍스트 분석 및 추천 폰트 적용 임시 구현
    - textarea에 입력한 텍스트를 분석한 폰트 추천 및 스타일 적용 구현
    - gpt 프롬프트 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,77 +3,101 @@
 import { useEffect, useRef, useState } from "react";
 
 interface Font {
-  name: string;
-  url: string;
+  fontKeywords: string[];
+  fontName: string;
+  fontUrl: string;
+  fontColor: string;
+  backgroundColor: string;
 }
 
-// 테스트용 더미 데이터
-const fonts: Font[] = [
-  {
-    name: "Abeezee",
-    url: "http://themes.googleusercontent.com/static/fonts/abeezee/v1/JYPhMn-3Xw-JGuyB-fEdNA.ttf",
-  },
-  {
-    name: "Lora",
-    url: "https://fonts.gstatic.com/s/bitter/v7/HEpP8tJXlWaYHimsnXgfCOvvDin1pK8aKteLpeZ5c0A.woff2",
-  },
-];
+interface StyleRecommend {
+  abstract: string;
+  keywords: string[];
+  suggestions: Font[];
+}
 
-const MESSAGE_1 =
-  "모든 사람은 의견의 자유와 표현의 자유에 대한 권리를 가진다. 이러한 권리는 간섭없이 의견을 가질 자유와 국경에 관계없이 어떠한 매체를 통해서도 정보와 사상을 추구하고, 얻으며, 전달하는 자유를 포함한다. 모든 사람은 사회의 일원으로서 사회보장을 받을 권리를 가지며, 국가적 노력과 국제적 협력을 통하여, 그리고 각 국가의 조직과 자원에 따라서 자신의 존엄과 인격의 자유로운 발전에 불가결한 경제적, 사회적 및 문화적 권리들을 실현할 권리를 가진다. 모든 사람은  노동시간의 합리적 제한과 정기적인 유급휴가를 포함하여 휴식과 여가의 권리를 가진다. 모든 사람은 이 선언에 규정된 권리와 자유가 완전히 실현될 수 있도록 사회적, 국제적 질서에 대한 권리를 가진다";
+// const MESSAGE_1 =
+//   "모든 사람은 의견의 자유와 표현의 자유에 대한 권리를 가진다. 이러한 권리는 간섭없이 의견을 가질 자유와 국경에 관계없이 어떠한 매체를 통해서도 정보와 사상을 추구하고, 얻으며, 전달하는 자유를 포함한다. 모든 사람은 사회의 일원으로서 사회보장을 받을 권리를 가지며, 국가적 노력과 국제적 협력을 통하여, 그리고 각 국가의 조직과 자원에 따라서 자신의 존엄과 인격의 자유로운 발전에 불가결한 경제적, 사회적 및 문화적 권리들을 실현할 권리를 가진다. 모든 사람은  노동시간의 합리적 제한과 정기적인 유급휴가를 포함하여 휴식과 여가의 권리를 가진다. 모든 사람은 이 선언에 규정된 권리와 자유가 완전히 실현될 수 있도록 사회적, 국제적 질서에 대한 권리를 가진다";
 
-const MESSAGE_2 =
-  "숲 속에 두 갈래 길이 있었다. 나는 사람들이 덜 간 길을 택했다. 그리고 그것으로 모든 것이 달라졌다.";
+// const MESSAGE_2 =
+//   "숲 속에 두 갈래 길이 있었다. 나는 사람들이 덜 간 길을 택했다. 그리고 그것으로 모든 것이 달라졌다.";
 
 const MESSAGE_3 =
   "... 그리고 내가 눈으로 그에게 물었다 내게 다시 그래요를 요구하겠느냐고 그러자 그는 내게 물었다 내가 그래요라고 말하겠는가 하고 그래요 나의 야산의 꽃이여 그리고 나는 처음으로 그의 목을 팔로 껴안고 그를 나에게 끌어당겼다 향기를 풍기는 나의 젖가슴을 그가 느낄 수 있도록 말이다 그래요 그러자 그의 심장은 미친 듯이 뛰었다 그래서 나는 그래요 하고 말했다 그렇게 하겠어요 그래요.";
 
 const Home = () => {
   const mainRef = useRef<HTMLElement>(null);
-  const [font, setFont] = useState<Font | undefined>();
+  const [font, setFont] = useState<StyleRecommend | undefined>();
+  const [value, setValue] = useState("");
+  const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  // 1. font 정보 동적 변경 적용 여부 확인하기
-  // TODO: 커스텀 훅으로 분리
-  // useEffect(() => {
-  //   const fetchFont = async (font: Font) => {
-  //     const newFont = new FontFace(`${font.name}`, `url(${font.url})`);
-  //     document.fonts.add(newFont);
-  //     // await document.fonts.ready;
-  //     await newFont.load();
-  //     if (mainRef.current) mainRef.current.style.fontFamily = `${font.name}`;
-  //   };
-
-  //   if (font) fetchFont(font);
-  // });
+  const handleFontStyleClick = async (fontName: string, fontUrl: string) => {
+    const newFontFace = new FontFace(`${fontName}`, `url(${fontUrl})`);
+    document.fonts.add(newFontFace);
+    await newFontFace.load();
+    if (mainRef.current) {
+      mainRef.current.style.fontFamily = `${fontName}`;
+    }
+  };
 
   useEffect(() => {
-    const fetchFontUrl = async (message: string) => {
-      const response = await fetch("/api/chat", {
+    const fetchFontUrlBy = async (message: string) => {
+      setIsLoading(true);
+      const response = await fetch("api/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ message }),
       });
+
       const { data } = await response.json();
-      console.log(data);
+      setFont(data);
+      setIsLoading(false);
     };
 
-    fetchFontUrl(MESSAGE_3);
-  }, []);
+    fetchFontUrlBy(message);
+  }, [message]);
 
   return (
     <main ref={mainRef}>
-      <h1>Hello</h1>
-      {fonts.map((font) => (
-        <button
-          key={font.url}
-          onClick={() => {
-            setFont(font);
-          }}
-        >
-          {font.name}
-        </button>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          setMessage(value);
+        }}
+      >
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <label htmlFor="context">분석할 텍스트를 입력해주세요.</label>
+          {isLoading ? (
+            <p>분석중입니다</p>
+          ) : (
+            <textarea
+              id="context"
+              name="context"
+              cols={5}
+              rows={15}
+              value={value}
+              onChange={(e) => {
+                setValue(e.target.value);
+              }}
+            ></textarea>
+          )}
+          <button type="submit">분석하기</button>
+        </div>
+      </form>
+
+      {font?.suggestions.map(({ fontName, fontUrl }) => (
+        <div key={fontUrl}>
+          <button
+            onClick={() => {
+              handleFontStyleClick(fontName, fontUrl);
+            }}
+          >
+            <p>{fontName}</p>
+          </button>
+        </div>
       ))}
     </main>
   );
